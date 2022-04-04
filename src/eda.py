34c869f75e4b51a5873c475baf5f7854dfbe70c9
Anna <anna.dai@duke.edu>
2022-04-03 21:00:48 -0400

    some stuff

--- src/eda.py
+++ src/eda.py
@@ -7,6 +7,20 @@ import seaborn as sns
 from sklearn.experimental import enable_iterative_imputer  # experimental feature
 from sklearn.impute import IterativeImputer
 
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import mean_squared_error
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.model_selection import RandomizedSearchCV
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import KFold
+# from sklearn.model_selection import learning_curve
+# from sklearn.model_selection import ShuffleSplit
+# from sklearn.model_selection import validation_curve
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import cross_val_predict
+# from sklearn.model_selection import cross_val_score
+
+
 # %%
 # read in data
 df = pd.read_csv("../artifacts/cleandata.csv")
@@ -28,3 +42,8 @@ df["recommend"].value_counts()
 # %%
 # impute missing values
 df["bust_size_num"].fillna(df.bust_size_num.mean(), inplace=True)
+
+# %%
+# check those with missing bust_size_num
+df[df.bust_size_num.isnull()]
+# %%
