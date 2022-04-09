6098fc34dc690f55ed6ad66279042b4ffe4c9d2d
Anna <anna.dai@duke.edu>
2022-04-09 14:08:23 -0400

    changes

--- src/mf.py
+++ src/mf.py
@@ -9,10 +9,11 @@ from surprise.prediction_algorithms.knns import KNNBasic
 from surprise.dataset import Dataset
 from surprise import Reader
 from surprise.prediction_algorithms.matrix_factorization import SVD
-from surprise import accuracy
-from sklearn.model_selection import train_test_split
 
-# from helperfunctions import accuracy, ranking
+# from surprise import accuracy
+from surprise.model_selection.split import train_test_split
+
+from helperfunctions import accuracy, ranking
 
 
 # %%
@@ -28,14 +29,16 @@ dfs = [train, val, test]
 
 # %%
 # Load data into surprise
-datasets = []
-for df in dfs:
+ds_names = ["train", "val", "test"]
+datasets = {}
+for i, df in enumerate(dfs):
     df_cluster = df[["user_id", "item_id", "recommend"]].rename(
-        {"user_id": "userID", "item_id": "itemID"}, axis=1
+        {"user_id": "userID", "item_id": "itemID", "recommend": "rating"}, axis=1
     )
     reader = Reader(rating_scale=(0, 1))
     ds = Dataset.load_from_df(df_cluster, reader)
-    datasets.append(ds)
+
+    datasets[ds_names[i]] = ds
 
 
 # %%
@@ -44,11 +47,11 @@ for df in dfs:
 ###############################################################################
 ######################## FIT SVD (MATRIX FACTORIZATION) #######################
 
-IWANTTORERUNMF = False
+IWANTTORERUNMF = True
 
 if IWANTTORERUNMF:
     mf = SVD()
-    mf.fit(train)
+    mf.fit(datasets["train"])
     # save to disk
     with open("mf_model.pkl", "wb") as file:
         pickle.dump(mf, file)
@@ -58,13 +61,13 @@ else:
         mf = pickle.load(file)
 
 
-preds_mf = mf.test(test)
+preds_mf = mf.test(datasets["test"])
 
 
 #%%
 # Check accuracy of mf
 accuracy(preds_mf)
-
+# 65% similar to random chance
 
 # %%
 ###############################################################################
@@ -73,26 +76,48 @@ accuracy(preds_mf)
 # import from disk
 df_reduced = pd.read_parquet("../artifacts/reduceddata.parquet")
 
-# Train/Test split
-train_reduced, test_reduced = train_test_split(
-    df_reduced, test_size=0.3, random_state=42
-)
+# # Train/Test split
+# train_reduced, test_reduced = train_test_split(
+#     df_reduced, test_size=0.3, random_state=42
+# )
 
-dfs = [train_reduced, test_reduced]
+# dfs = [train_reduced, test_reduced]
 
 # Load data into surprise
-datasets = []
-for df in dfs:
-    df_cluster = df[["user_id", "item_id", "recommend"]].rename(
-        {"user_id": "userID", "item_id": "itemID"}, axis=1
-    )
-    reader = Reader(rating_scale=(0, 1))
-    ds = Dataset.load_from_df(df_cluster, reader)
-    datasets.append(ds)
+# ds_names = ["train_reduced", "test_reduced"]
+# datasets = []
+# for df in dfs:
+#     df_cluster = df[["user_id", "item_id", "recommend"]].rename(
+#         {"user_id": "userID", "item_id": "itemID"}, axis=1
+#     )
+#     reader = Reader(rating_scale=(0, 1))
+#     ds = Dataset.load_from_df(df_cluster, reader)
+#     datasets.append(ds)
+
+
+df_cluster = df_reduced[["user_id", "item_id", "recommend"]].rename(
+    {"user_id": "userID", "item_id": "itemID"}, axis=1
+)
+reader = Reader(rating_scale=(0, 1))
+ds = Dataset.load_from_df(df_cluster, reader)
+train_reduced, test_reduced = train_test_split(ds, test_size=0.25, random_state=42)
 
 #%%
-
 mf_reduced = SVD()
-mf_reduced.fit(train)
+mf_reduced.fit(train_reduced)
+
+# %%
+mf_reduced.test(test_reduced)
+accuracy(mf_reduced.test(test_reduced))
+# 63.5% lol
+
+# %%
+
+# train test split from sklearn
+from sklearn.model_selection import train_test_split
+
+train_r, test_r = train_test_split(df_reduced, test_size=0.25, random_state=42)
+
+# NOTE: CONFIRM NOT THE SAME AS SURPRISE SPLIT
 
 # %%
